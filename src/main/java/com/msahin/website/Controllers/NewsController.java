b7fe414205e965da6b1bb195848801857291bd0f
Mehmet <mehmetsahinn2002@gmail.com>
2023-03-09 22:37:45 +0300

    Added add-news page / redesigned Main Page

--- src/main/java/com/msahin/website/Controllers/NewsController.java
+++ src/main/java/com/msahin/website/Controllers/NewsController.java
@@ -2,11 +2,22 @@ package com.msahin.website.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 class NewsController {
      @GetMapping("/")
     public String getNews() {
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String addNews() {
+        return "add-news";
+    }
+
+    @ModelAttribute("navbar")
+    public String navbar() {
+        return "nav";
     }
 }
